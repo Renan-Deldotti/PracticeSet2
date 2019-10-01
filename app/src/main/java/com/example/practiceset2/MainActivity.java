1f783772a0cbad96bae03b44d6af1bc199b83a2b
Renan_Deldotti <renan.deldotti@hotmail.com>
2019-10-01 19:58:03 -0300

    Changes

--- app/src/main/java/com/example/practiceset2/MainActivity.java
+++ app/src/main/java/com/example/practiceset2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.practiceset2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addPoint(View view) {
+        int newScore = 0;
+        switch (view.getId()) {
+            case R.id.freeThrowButton:
+                newScore = 1;
+                break;
+            case R.id.twoPointsButton:
+                newScore = 2;
+                break;
+            case R.id.threePointsButton:
+                newScore = 3;
+                break;
+        }
+        TextView scoreView = findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(newScore + Integer.parseInt(scoreView.getText().toString())));
+
     }
 }
