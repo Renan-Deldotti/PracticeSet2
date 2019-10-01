9a9813f7418114df6061d7ad7bd8f00b7f3f2706
Renan_Deldotti <renan.deldotti@hotmail.com>
2019-10-01 18:46:21 -0300

    Add buttons

--- app/src/main/java/com/example/practiceset2/MainActivity.java
+++ app/src/main/java/com/example/practiceset2/MainActivity.java
@@ -12,27 +12,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void display(String text) {
-        TextView t = (TextView) findViewById(R.id.display_text_view);
-        t.setText(text);
-    }
-
-    public void display(int text) {
-        TextView t = (TextView) findViewById(R.id.display_text_view);
-        t.setText(text + "");
-    }
-
-    public void display1(String text) {
-        display(text);
-    }
-
-    public void display2(String text) {
-        TextView t = (TextView) findViewById(R.id.display_text_view_2);
-        t.setText(text);
-    }
-
-    public void display3(String text) {
-        TextView t = (TextView) findViewById(R.id.display_text_view_3);
-        t.setText(text);
-    }
 }
