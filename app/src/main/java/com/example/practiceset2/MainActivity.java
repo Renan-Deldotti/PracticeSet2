35f62b10ea79ddc415568a43665e20827fd86f38
Renan_Deldotti <renan.deldotti@hotmail.com>
2019-10-01 21:27:49 -0300

    Reset Button
    Reset button was added.

--- app/src/main/java/com/example/practiceset2/MainActivity.java
+++ app/src/main/java/com/example/practiceset2/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addPoint(View view) {
+        // Receive a view and find which one of the buttons that sent the action
+        char team = 'a';
         int newScore = 0;
         switch (view.getId()) {
             case R.id.freeThrowButton:
@@ -26,9 +28,36 @@ public class MainActivity extends AppCompatActivity {
             case R.id.threePointsButton:
                 newScore = 3;
                 break;
+            case R.id.freeThrowButtonB:
+                team = 'b';
+                newScore = 1;
+                break;
+            case R.id.twoPointsButtonB:
+                team = 'b';
+                newScore = 2;
+                break;
+            case R.id.threePointsButtonB:
+                team = 'b';
+                newScore = 3;
+                break;
         }
-        TextView scoreView = findViewById(R.id.team_a_score);
+        // Test if it is team 'A' or 'B'
+        TextView scoreView;
+        if (team == 'a') {
+            scoreView = findViewById(R.id.team_a_score);
+        } else {
+            scoreView = findViewById(R.id.team_b_score);
+        }
+        //Updates the value of the TextView
         scoreView.setText(String.valueOf(newScore + Integer.parseInt(scoreView.getText().toString())));
 
+    }
+
+    public void resetPoints(View view) {
+        TextView scoreView;
+        scoreView = findViewById(R.id.team_a_score);
+        scoreView.setText("0");
+        scoreView = findViewById(R.id.team_b_score);
+        scoreView.setText("0");
     }
 }
